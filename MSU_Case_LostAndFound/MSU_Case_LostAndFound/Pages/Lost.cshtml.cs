aa4ea9104755c2489978d27fb514bf35b2707ed1
Jørgen Jærnes <jorgen.jarnes@communicate.no>
2020-08-14 14:38:27 +0200

    front-end merge

---
+++ MSU_Case_LostAndFound/MSU_Case_LostAndFound/Pages/Lost.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace MSU_Case_LostAndFound.Pages
+{
+    public class LostModel : PageModel
+    {
+        private readonly ILogger<LostModel> _logger;
+
+        public LostModel(ILogger<LostModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
