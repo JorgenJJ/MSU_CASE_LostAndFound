4115ba044e6355c93651a30e129a02bd988947ba
Axel Grefslie <agrefs97@gmail.com>
2020-08-24 15:03:52 +0200

    added sign out feature

--- MSU_Case_LostAndFound/MSU_Case_LostAndFound/Startup.cs
+++ MSU_Case_LostAndFound/MSU_Case_LostAndFound/Startup.cs
@@ -25,11 +25,7 @@ namespace MSU_Case_LostAndFound
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddAuthentication(
-                CertificateAuthenticationDefaults.AuthenticationScheme)
-                .AddCertificate();
-
-
+           
             services.AddAuthentication().AddEasyAuthAuthentication((o) => { });
 
             services.AddRazorPages();
