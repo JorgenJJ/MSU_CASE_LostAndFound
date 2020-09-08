dcdb3b7d104387384cea81ce2bb3cad109bcfd4e
g0ve <patrick.gove@hotmail.com>
2020-09-08 11:48:17 +0200

    Revert "added google signin"
    This reverts commit 06e509d8c781dc2bc71e8e079ce68e9bf1e13ca4.

--- MSU_Case_LostAndFound/MSU_Case_LostAndFound/Startup.cs
+++ MSU_Case_LostAndFound/MSU_Case_LostAndFound/Startup.cs
@@ -29,17 +29,6 @@ namespace MSU_Case_LostAndFound
 
             services.AddRazorPages();
 
-
-
-            services.AddAuthentication().AddGoogle(options =>
-            {
-                IConfigurationSection googleAuthNSection =
-                    Configuration.GetSection("Authentication:Google");
-
-                options.ClientId = googleAuthNSection["ClientId"];
-                options.ClientSecret = googleAuthNSection["ClientSecret"];
-            });
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
