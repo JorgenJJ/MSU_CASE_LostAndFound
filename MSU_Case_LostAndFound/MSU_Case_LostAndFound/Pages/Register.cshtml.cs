51a92ecc351950957c11f85b8c3321a2b95f079e
g0ve <patrick.gove@hotmail.com>
2020-09-10 10:37:14 +0200

    Register

---
+++ MSU_Case_LostAndFound/MSU_Case_LostAndFound/Pages/Register.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using MSU_Case_LostAndFound.Models;
+
+namespace MSU_Case_LostAndFound.Pages
+{
+    public class RegisterModel : PageModel
+    {
+        private readonly RescuteDBContext _db;
+
+        public RegisterModel(RescuteDBContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public AnimalsLost AnimalsLost { get; set; }
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                await _db.AnimalsLost.AddAsync(AnimalsLost);
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Lost");
+
+            }
+            else
+            {
+                return Page();
+            }
+        }
+    }
+}
