cc3a289b21fb5a229c74457dc8818eba63f80d97
JorgenJJ <jorgen.jarnes@gmail.com>
2020-09-17 10:04:11 +0200

    Copied Lost page into Found

--- MSU_Case_LostAndFound/MSU_Case_LostAndFound/Pages/Found.cshtml.cs
+++ MSU_Case_LostAndFound/MSU_Case_LostAndFound/Pages/Found.cshtml.cs
@@ -4,13 +4,69 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using MSU_Case_LostAndFound.Model;
+
 
 namespace MSU_Case_LostAndFound.Pages
 {
     public class FoundModel : PageModel
     {
-        public void OnGet()
+        public IEnumerable<AnimalsLost> AnimalLst { get; set; }
+        //public string CurrentFilter { get; set; }
+
+
+        private readonly RescuteDBContext _db;
+        public FoundModel(RescuteDBContext db)
         {
+            _db = db;
         }
+
+        [BindProperty]
+        public AnimalsLost AnimalsFound { get; set; }
+        public async Task OnGetAsync(string searchString, string searchAnimal)
+        {
+
+            //CurrentFilter = searchString;
+
+            IQueryable<AnimalsLost> studentsIQ = from s in _db.AnimalsLost
+                                                 select s;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                studentsIQ = studentsIQ.Where(s => s.Name.Contains(searchString));
+            }
+
+            if (!String.IsNullOrEmpty(searchAnimal))
+            {
+                //AnimalsLost.AnimalId myValueAsEnum = (AnimalsLost.Animal)
+                //              Enum.Parse(typeof(AnimalsLost.Animal), searchAnimal);
+
+                studentsIQ = studentsIQ.Where(s => s.Animal == (Animal)Enum.Parse(typeof(Animal), searchAnimal));
+            }
+
+            AnimalLst = await studentsIQ.AsNoTracking().ToListAsync();
+
+        }
+
+
+
+
+        //public async Task<IActionResult> OnPost()
+        //{
+
+        //if (ModelState.IsValid)
+        //{
+        //    await _db.AnimalsLost.AddAsync(AnimalsLost);
+        //    await _db.SaveChangesAsync();
+        //    return RedirectToPage("Lost");
+
+        //}
+        //else
+        //{
+        //    return Page();
+        //}
+        //}
     }
 }
