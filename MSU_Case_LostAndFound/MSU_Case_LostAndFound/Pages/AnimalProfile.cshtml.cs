d34d4f021d667f9e8aa71e8af98f4ffe7b9d16ca
g0ve <patrick.gove@hotmail.com>
2020-09-17 14:59:53 +0200

    phone+name in animalprofile

--- MSU_Case_LostAndFound/MSU_Case_LostAndFound/Pages/AnimalProfile.cshtml.cs
+++ MSU_Case_LostAndFound/MSU_Case_LostAndFound/Pages/AnimalProfile.cshtml.cs
@@ -2,12 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using MSU_Case_LostAndFound.Areas.Identity.Data;
 using MSU_Case_LostAndFound.Model;
 
 namespace MSU_Case_LostAndFound.Pages
@@ -15,11 +17,19 @@ namespace MSU_Case_LostAndFound.Pages
     public class AnimalProfileModel : PageModel
     {
         public AnimalsLost animal;
+        public ApplicationUser user;
+
+        public string userName;
+        public string userPhoneNumber;
 
         private readonly RescuteDBContext _db;
-        public AnimalProfileModel(RescuteDBContext db)
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AnimalProfileModel(RescuteDBContext db, UserManager<ApplicationUser> userManager)
         {
             _db = db;
+            _userManager = userManager;
+
         }
 
 
@@ -30,6 +40,12 @@ namespace MSU_Case_LostAndFound.Pages
         {
             var temp_id = RouteData.Values["id"];
             animal = await _db.AnimalsLost.FindAsync(int.Parse((string)temp_id));
+
+            var user = await _userManager.FindByIdAsync(animal.UserId);
+
+            userName = user.firstname + user.lastname;
+            userPhoneNumber = user.PhoneNumber;
+
         }
     }
 }
