cb5f05d0131ba37be2f51aa2714030ac198c3f6f
Axel Grefslie <agrefs97@gmail.com>
2020-09-17 15:37:40 +0200

    Revert "Added keyvault"
    This reverts commit d6bdab0be0c2fe9c9fef36de8021da292213cbbf.

--- MSU_Case_LostAndFound/MSU_Case_LostAndFound/Program.cs
+++ MSU_Case_LostAndFound/MSU_Case_LostAndFound/Program.cs
@@ -1,12 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Azure.Identity;
 
 
 namespace MSU_Case_LostAndFound
@@ -19,16 +13,8 @@ namespace MSU_Case_LostAndFound
         
         }
 
-       
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration((context, config) =>
-                {
-                    var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
-                    config.AddAzureKeyVault(
-                    keyVaultEndpoint,
-                    new DefaultAzureCredential());
-                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
