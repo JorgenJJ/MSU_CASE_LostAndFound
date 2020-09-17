74217045b323da21f63bbea0a3820ba0c2792d23
Axel Grefslie <agrefs97@gmail.com>
2020-09-17 13:51:56 +0200

    added managed layout

---
+++ MSU_Case_LostAndFound/MSU_Case_LostAndFound/Areas/Identity/Data/MSU_Case_LostAndFoundContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MSU_Case_LostAndFound.Data
+{
+    public class MSU_Case_LostAndFoundContext : IdentityDbContext<IdentityUser>
+    {
+        public MSU_Case_LostAndFoundContext(DbContextOptions<MSU_Case_LostAndFoundContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
