da875803eb9146d814ba206bdbb98ac9617cd54d
Axel Grefslie <agrefs97@gmail.com>
2020-09-17 15:30:19 +0200

    Revert "Merge branch 'master' of https://github.com/JorgenJJ/MSU_CASE_LostAndFound"
    This reverts commit 5c8b1b6d727420082f9e848e8f0de561f45444d7, reversing
    changes made to d6bdab0be0c2fe9c9fef36de8021da292213cbbf.

--- MSU_Case_LostAndFound/MSU_Case_LostAndFound/Pages/AnimalProfile.cshtml.cs
+++ MSU_Case_LostAndFound/MSU_Case_LostAndFound/Pages/AnimalProfile.cshtml.cs
@@ -2,14 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
-using MSU_Case_LostAndFound.Areas.Identity.Data;
 using MSU_Case_LostAndFound.Model;
 
 namespace MSU_Case_LostAndFound.Pages
@@ -17,19 +15,11 @@ namespace MSU_Case_LostAndFound.Pages
     public class AnimalProfileModel : PageModel
     {
         public AnimalsLost animal;
-        public ApplicationUser user;
-
-        public string userName;
-        public string userPhoneNumber;
 
         private readonly RescuteDBContext _db;
-        private readonly UserManager<ApplicationUser> _userManager;
-
-        public AnimalProfileModel(RescuteDBContext db, UserManager<ApplicationUser> userManager)
+        public AnimalProfileModel(RescuteDBContext db)
         {
             _db = db;
-            _userManager = userManager;
-
         }
 
 
@@ -40,12 +30,6 @@ namespace MSU_Case_LostAndFound.Pages
         {
             var temp_id = RouteData.Values["id"];
             animal = await _db.AnimalsLost.FindAsync(int.Parse((string)temp_id));
-
-            var user = await _userManager.FindByIdAsync(animal.UserId);
-
-            userName = user.firstname + user.lastname;
-            userPhoneNumber = user.PhoneNumber;
-
         }
     }
 }
